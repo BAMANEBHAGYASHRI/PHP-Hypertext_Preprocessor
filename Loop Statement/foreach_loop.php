7a3da7e9e8c61774fc32324697d5c0bbbc04c663
BHAGYASHRI <bhagyashribamaneb2004@gmail.com>
2022-06-30 15:58:43 +0530

    Loop Statements-PHP

---
+++ Loop Statement/foreach_loop.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $student =array("roll no"=>"101","name"=>"bhagyashri","marks"=>"90");
+    foreach($student as $key=>$value){
+        echo "$key-$value";
+        
+    }
+
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
