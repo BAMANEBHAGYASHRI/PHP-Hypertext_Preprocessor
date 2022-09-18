5087662a01d223f7a21a8626479c7032ea2651e2
BHAGYASHRI <shrutibamaneb@gmail.com>
2022-09-18 22:54:55 +0530

    Function-PHP

---
+++ Function/1.1Simple_Function.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<?php
+// Defining a PHP functions
+ function writeMessage()
+ {
+ echo “Welcome to PHP world”;
+ } 
+// Calling a PHP function.
+writeMessage();
+?>
+</body>
+</html>
